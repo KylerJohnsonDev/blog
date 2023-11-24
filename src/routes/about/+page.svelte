70bd36938f9f3dca9db929d731a2c35e13b8457b
Kyler Johnson <kjohnson@octolabs.io>
2023-11-24 17:24:15 -0500

    fill out about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,36 @@
-About page
+<script>
+    import portrait from '$lib/assets/kyler_johnson.jpg';
+
+</script>
+
+<main class="mb-auto">
+    <div class="divide-y divide-gray-200 dark:divide-gray-700">
+        <div class="space-y-2 pt-6 pb-8 md:space-y-5"><h1
+                class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            About</h1></div>
+        <div class="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+            <div class="flex flex-col items-center pt-8">
+
+                <span
+                    style="box-sizing: border-box; display: inline-block; overflow: hidden; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; position: relative; max-width: 100%;">
+                    <span style="box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;">
+                    <img class="h-36 w-36 rounded-full"
+                    style="display: block; max-width: 100%; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;"
+                    alt="" aria-hidden="true"
+                    src={portrait}></span><img
+                    alt="avatar" src={portrait}
+                    decoding="async" data-nimg="intrinsic" class="h-36 w-36 rounded-full"
+                    style="position: absolute; inset: 0px; box-sizing: border-box; padding: 0px; border: medium; margin: auto; display: block; width: 0px; height: 0px; min-width: 100%; max-width: 100%; min-height: 100%; max-height: 100%;"
+                    srcset={portrait}></span>
+                <h3 class="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">Kyler Johnson</h3>
+                <div class="text-gray-500 dark:text-gray-400">Senior Principal Software Engineer</div>
+                <div class="text-gray-500 dark:text-gray-400">Secureworks</div>
+            </div>
+            <div class="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">
+                <p>
+                    Kyler Johnson is a Software Craftsman specializing in Nx, Angular, Node.js, and .NET technologies. Currently working as a Senior Principal Engineer, he has years of experience building and maintaining enterprise-scale SaaS products.
+                </p>
+            </div>
+        </div>
+    </div>
+</main>
